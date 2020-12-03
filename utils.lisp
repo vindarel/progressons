40fcfda232b96cd9f2778cc25aaf924e3e1038dd
vindarel <vindarel@mailz.org>
2020-12-03 06:41:35 +0100

    (minor) utils: simpler tty-p

--- utils.lisp
+++ utils.lisp
@@ -7,7 +7,7 @@
 (defun tty-p (&key force)
   "Return T if we run on a terminal.
   This must fail on Slime (on Emacs' default shell prompt) and succeed on a Lisp in a terminal window."
-  (when (and (not force)
-             (boundp '*tty-p*))
-    (return-from tty-p *tty-p*))
-  (setf *tty-p* (not (equalp "dumb" (uiop:getenv "TERM")))))
+  (if (and (not force)
+           (boundp '*tty-p*))
+      *tty-p*
+      (setf *tty-p* (not (equalp "dumb" (uiop:getenv "TERM"))))))
