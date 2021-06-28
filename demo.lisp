68704f2ff39e49eb584d9d4f38d4ff243860d313
vindarel <vindarel@mailz.org>
2021-06-28 20:03:26 +0200

    (minor) demo: with termp, don't quit on Slime

--- demo.lisp
+++ demo.lisp
@@ -1,9 +1,10 @@
 (load "progressons.asd")
-(ql:quickload "progressons")
+(ql:quickload '("progressons" "termp"))
 
 (in-package :progressons)
 (loop for line in (progressbar (list 1 2 3 4 5)) do (sleep 0.3) (step!))
 
 (loop for elt in (progressbar (loop for i from 1 to 200 collect i)) do (sleep 0.005) (step!))
 
-(uiop:quit)
+(when (termp:termp)
+  (termp:quit))
